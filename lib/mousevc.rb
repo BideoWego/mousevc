fc092c62427dce120fef5ec17d656c6f584f95d5
Chris Scavello <bideowego@gmail.com>
2015-08-19 20:25:34 -0400

    Update documentation

--- lib/mousevc.rb
+++ lib/mousevc.rb
@@ -1,9 +1,24 @@
 require "mousevc/version"
 require "mousevc/app"
 
+##
+# ===Mouse
+#
+# 	(`) (`)
+# 	=('o')=
+# 	  m m  
+#
+# 	MousevC
+# 	V     L
+# 	C     I
+# 
+
 module Mousevc
-  # Your code goes here...
-  def self.art
-  	print "\n(`) (`)\n=('o')=\n  m m  \n\nMousevC\nV     L\nC     I\n\n"
-  end
+
+	##
+	# Outputs some pretty ASCII art
+
+	def self.art
+		print "\n(`) (`)\n=('o')=\n  m m  \n\nMousevC\nV     L\nC     I\n\n"
+	end
 end
--- lib/mousevc/app.rb
+++ lib/mousevc/app.rb
@@ -2,18 +2,46 @@ require_relative 'router.rb'
 require_relative 'input.rb'
 require_relative 'error.rb'
 
-##
-#
-# (`) (`)
-# =('o')=
-#   m m  
-# MousevC
-# V     L
-# C     I
-# 
-
 module Mousevc
+
+	##
+	# App is the top level class of Mousevc.
+	# App is the container for all of the objects
+	# created within a Mousevc application.
+
 	class App
+
+		##
+		# Creates a new App instance
+		#
+		# Expects a parameter of type `Hash`
+		# with the following format:
+		# * +:controller+ -> string name of
+		# the default controller class. This
+		# is the controller that will be 
+		# instantiated at the beginning of
+		# your program.
+		# * +:model+ -> string name of the
+		# model that corresponds to the
+		# default controller. This model
+		# will be available to the controller
+		# via it's @model attribute.
+		# * +:action+ -> symbol name of the
+		# method to call on the default
+		# controller at run time. This method
+		# will be the entry point for your
+		# application.
+		# * +:views+ -> The relative path to
+		# your views directory .e.g if your
+		# application lives in +app/+ and your
+		# views live in a sub-directory of
+		# +app/+ called +views+ then this
+		# parameter should be +'views'+.
+		# 
+		# * *Params*:
+		# 	- +options+ -> the options hash
+		#
+
 		def initialize(options={})
 			@controller = options[:controller]
 			@model = options[:model]
