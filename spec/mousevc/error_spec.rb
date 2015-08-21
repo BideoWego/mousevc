1b38a4457ed657149084826d41eb65aeba2e982d
Chris Scavello <bideowego@gmail.com>
2015-08-21 18:02:03 -0400

    Complete Error and Input tests

--- spec/mousevc/error_spec.rb
+++ spec/mousevc/error_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe Mousevc::Error do
-	it 'holds the given error message'
+	it 'holds the given error message' do
+		expect {raise Mousevc::Error.new("Boom!")}.to raise_error(Mousevc::Error, "Boom!")
+	end
 end
--- spec/mousevc/input_spec.rb
+++ spec/mousevc/input_spec.rb
@@ -1,48 +1,131 @@
 require 'spec_helper'
 
 describe Mousevc::Input do
+	before do
+		Mousevc::Input.clear
+	end
+
 	describe '#clear' do
-		it 'clears all class variables by setting them to nil'
-		it 'clears specific class variables from a symbol list'
+		it 'sets all class variables to their original values' do
+			Mousevc::Input.notice = 'Notable notes'
+			Mousevc::Input.prompts.store(:dollar, '$')
+			Mousevc::Input.appearance = :dollar
+			Mousevc::Input.clear
+			expect(Mousevc::Input.notice).to eq(nil)
+			expect(Mousevc::Input.prompts).to eq({:default => '>'})
+			expect(Mousevc::Input.appearance).to eq(nil)
+		end
+
+		it 'clears specific class variables from a symbol list' do
+			Mousevc::Input.notice = 'Notable notes'
+			Mousevc::Input.prompts.store(:dollar, '$')
+			Mousevc::Input.clear(:prompts)
+			expect(Mousevc::Input.notice).to eq('Notable notes')
+			expect(Mousevc::Input.prompts).to eq({:default => '>'})
+		end
 	end
 
 	describe '#prompts' do
-		it 'is read only'
-		it 'defaults to a hash with the default prompt'
+		it 'is read only' do
+			expect {Mousevc::Input.prompts = nil}.to raise_error
+		end
+
+		it 'defaults to a hash with the default prompt' do
+			expect(Mousevc::Input.prompts).to eq({:default => '>'})
+		end
 	end
 
 	describe '#appearance' do
-		it 'represents the default appearance of the prompt'
+		it 'represents the default appearance of the prompt' do
+			Mousevc::Input.prompts.store(:dollar, '$')
+			Mousevc::Input.appearance = :dollar
+			@output = WrapIO.of do
+				Mousevc::Input.prompt
+			end
+			expect(@output).to eq("\n$ ")
+		end
 	end
 
 	describe '#prompt' do
-		it 'prompts the user for input'
-		it 'returns the stripped user input'
-		it 'allows choosing the appearance of the prompt'
+		it 'prompts the user for input' do
+			@output = WrapIO.of do
+				expect($stdin).to receive(:gets)
+				Mousevc::Input.prompt
+			end
+			expect(@output).to eq("\n> ")
+		end
+
+		it 'returns the stripped user input' do
+			WrapIO.of(['      q      ']) do
+				@input = Mousevc::Input.prompt
+			end
+			expect(@input).to eq('q')
+		end
+
+		it 'allows choosing the appearance of the prompt' do
+			Mousevc::Input.prompts.store(:dollar, '$')
+			@output = WrapIO.of do
+				Mousevc::Input.prompt(:dollar)
+			end
+			expect(@output).to eq("\n$ ")
+		end
 	end
 
 	describe '#notice' do
-		it 'returns the current notice'
-		it 'sets the notice'
+		it 'sets and gets the notice' do
+			Mousevc::Input.notice = 'Notable notes!'
+			expect(Mousevc::Input.notice).to eq('Notable notes!')
+		end
 	end
 
 	describe '#data' do
-		it 'is read only'
-		it 'returns the current user input'
+		it 'is read only' do
+			expect {Mousevc::Input.data = 'Raise the roof!'}.to raise_error
+		end
+
+		it 'returns the current user input' do
+			WrapIO.of do
+				Mousevc::Input.prompt
+				@data = Mousevc::Input.data
+			end
+			expect(@data).to eq('q')
+		end
 	end
 
 	describe '#notice?' do
-		it 'returns true if there is a notice'
-		it 'returns false if notice is nil or empty'
+		it 'returns true if there is a notice' do
+			Mousevc::Input.notice = "I'm a gonna notify you!"
+			expect(Mousevc::Input.notice?).to eq(true)
+		end
+
+		it 'returns false if notice is nil or empty' do
+			expect(Mousevc::Input.notice?).to eq(false)
+		end
 	end
 
 	describe '#reset?' do
-		it 'returns true if the user wants to reset'
-		it 'returns false if the user does not want to reset'
+		it 'returns true if the user wants to reset' do
+			WrapIO.of(['r']) do
+				Mousevc::Input.prompt
+			end
+			expect(Mousevc::Input.reset?).to eq(true)
+		end
+
+		it 'returns false if the user does not want to reset' do
+			expect(Mousevc::Input.reset?).to eq(false)
+		end
 	end
 
 	describe '#quit' do
-		it 'returns true if the user wants to quit'
-		it 'returns false if the user does not want to quit'
+		it 'returns true if the user wants to quit' do
+			WrapIO.of do
+				Mousevc::Input.prompt
+			end
+			expect(Mousevc::Input.quit?).to eq(true)
+		end
+		
+		it 'returns false if the user does not want to quit' do
+			expect(Mousevc::Input.quit?).to eq(false)
+		end
 	end
 end
