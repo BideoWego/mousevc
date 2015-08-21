59204f3a391228150fbc446906c87c4593875d59
Chris Scavello <bideowego@gmail.com>
2015-08-21 03:23:40 -0400

    Prettify #art, add info, add #factory

--- lib/mousevc.rb
+++ lib/mousevc.rb
@@ -1,18 +1,47 @@
-require "mousevc/version"
-require "mousevc/app"
+require_relative 'mousevc/version.rb'
+require_relative 'mousevc/app.rb'
 
 ##
 # Mousevc is the top level module and namespace for the Mousevc framework.
-# 
 
 module Mousevc
 
 	##
-	# @todo Add author name and website to art
+	# @todo Add link to documentation
 	#
 	# @return [String] some pretty ASCII art
 
 	def self.art
-		"\n(`) (`)\n=('o')=\n  m m  \n\nMousevC\nV     L\nC     I\n\n"
+		lines = [
+			"",
+			"(`) (`)",
+			"=('o')=",
+			"  m m  ",
+			"",
+			"MousevC",
+			"V     L",
+			"C     I",
+			"",
+			"by",
+			"Bideo Wego",
+			"",
+			"http://bideowego.com",
+			""
+		]
+		width = lines.max.length
+		lines.map do |s|
+			s.center(width)
+		end.join("\n")
+	end
+
+	##
+	# Generates a Mousevc class constant ready for instantiation
+	#
+	# @return [Constant] the class constant
+
+	def self.factory(class_name)
+		"Mousevc::#{class_name}".split('::').inject(Object) do |object, string|
+			object.const_get(string)
+		end
 	end
 end
